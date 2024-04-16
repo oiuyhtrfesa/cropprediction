b8b3096df2d4ea0b93dd00e7565a939ba3a551fb
oiuyhtrfesa <109503372+oiuyhtrfesa@users.noreply.github.com>
2024-04-16 06:48:38 +0530

    Add files via upload

---
+++ crop_prediction_web/f/forms.py
@@ -0,0 +1,8 @@
+# forms.py
+# from django import forms
+# from .models import CropParameters
+
+# class CropParametersForm(forms.ModelForm):
+#     class Meta:
+#         model = CropParameters
+#         fields = ['N', 'P', 'K', 'temperature', 'humidity', 'ph','rainfall']
