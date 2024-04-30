8c612350a048a8f2dc4de25667dfdade1b06e2d5
oiuyhtrfesa <109503372+oiuyhtrfesa@users.noreply.github.com>
2024-04-30 18:07:25 +0530

    Delete crop_prediction_web directory

--- crop_prediction_web/f/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'f'
--- crop_prediction_web/f/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-# from django import forms
-# from .models import CropParameters
-
-# class CropParametersForm(forms.ModelForm):
-#     class Meta:
-#         model = CropParameters
-#         fields = ['N', 'P', 'K', 'temperature', 'humidity', 'ph','rainfall']
